f287c9e83eccb9ab6e6619b72873e204d9efae7f
badara1981 <badara.jammeh@hotmail.com>
2022-08-10 10:00:16 +0200

    Prettier

---
+++ exercise.py
@@ -0,0 +1,127 @@
+# do exercise here
+# upload to github for portfolio
+# thank me later when your git graph is as green as the python money u gonna earn
+a = 123
+b = type(a)
+print(a,b,sep=" is type of ")
+
+c = 589
+d = type(c)
+print(c,d,sep=" complete type of")
+
+e = 43.23
+hb = type(e)
+print(a,b,sep=" is type of ")
+
+
+a = (4-1j)
+b = type(a)
+print(a,b,sep=" is type of ")
+
+
+a = "Welcome to Barcelona"
+b = type(a)
+print(a,b,sep=" is type of ")
+
+a = True
+b = type(a)
+print(a,b,sep=" is type of ") 
+
+H = "hELLO wORLD"
+print(H)
+
+
+a = 123
+b = isinstance(a, int)
+print(a,b)
+
+c = 458
+d = isinstance(c,int)
+print(c,d)
+
+
+a = 42.23
+b = isinstance(a, float)
+print(a,b)
+
+
+
+a = True
+b = isinstance(a, bool)
+print(a,b)
+
+a = (4+2j)
+b = isinstance(a, complex)
+print(a,b)
+
+a = "hello"
+b = isinstance(a, str)
+print(a,b)
+
+
+a = 123
+b = isinstance(a, int)
+print(a,b)
+
+
+a = 42.23
+b = isinstance(a, float)
+print(a,b)
+
+a = True
+b = isinstance(a, bool)
+print(a,b)
+
+a = (4+2j)
+b = isinstance(a, complex)
+print(a,b)
+
+a = "hello"
+b = isinstance(a, str)
+print(a,b)
+
+
+g =  7/9
+print(round(g,7)) 
+
+
+"""4"""
+
+
+a = 123
+b = int
+c = isinstance(a, b)
+print("is",a, "in an instance of:",b,"?",c)
+
+
+
+a = 123
+b = int
+c = isinstance(a, b)
+print("is",a, "in an instance of bool?:",c)
+
+a = 42.23
+b = bool
+c = isinstance(a, b)
+print("is",a, "in an instance of float?",c)
+
+a = (4-1j)
+b = complex
+c = isinstance(a, b)
+print("is",a, "in an instance of complex?",c)
+
+a = True
+b = bool
+c = isinstance(a, b)
+print("is",a, "in an instance of bool",c)
+
+
+a = "how are you?"
+b = float
+c = isinstance(a, b)
+print("is",a, "in an instance of float",c)
+
+c = "We are going to Munich"
+d = float
+e = isinstance(c,d)
+print ("is",a,"in the instance of float", e )

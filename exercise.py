0875d57e8051962b4ed6d7f87a68c1f8d16908f9
badara1981 <badara.jammeh@hotmail.com>
2022-08-17 14:25:54 +0200

    added

--- exercise.py
+++ exercise.py
@@ -489,3 +489,47 @@ print("Area of the circle =", area)
 
 
 # Task 5
+
+import random
+random_no = random.randrange(0, 10)
+guess_no =int(input("guess a Number =>"))
+if random_no == guess_no:
+ print("Your guess is Correct, You won!")
+elif random_no > guess_no:
+    print("Your guest  is low") 
+elif random_no:
+    print("Your Guest is High")
+
+#Task 5 Ex 6
+
+temp = input("Input the  temperature you like to convert? (e.g., 45F, 102C etc.) : ")
+degree = int(temp[:-1])
+i_convention = temp[-1]
+
+if i_convention.upper() == "C":
+  result = int(round((9 * degree) / 5 + 32))
+  o_convention = "Fahrenheit"
+elif i_convention.upper() == "F":
+  result = int(round((degree - 32) * 5 / 9))
+  o_convention = "Celsius"
+else:
+  print("Input proper convention.")
+  quit()
+print("The temperature in", o_convention, "is", result, "degrees.")
+
+# solution 11
+
+censius = int(input("Enter the Temperature in the Degree  that  would like to convert:-"))
+fahrenheit = (censius * 9/5 )  + 32
+print("Converted  temperature is", fahrenheit)
+
+# Ex 5 Question 7
+#Your task is to write a Python program to construct the following pattern. Upper part should be done **in one line** of code without using a loop.  
+#Lower part can be done with any kind of loop **or** also with one line of code and without loops#
+
+n = 5
+for i in range(n):
+    for j in range(i,n):
+            print("",end="")
+for j in range(i +1):
+        print("*",end="")
